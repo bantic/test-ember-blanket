2f6fffeeea0e7c012275d26f09fd9aca8976e323
Cory Forsyth <cory.forsyth@gmail.com>
2015-10-25 15:10:58 -0400

    pretender to 0.10.1, custom fork of ember-cli-blanket
      * pin pretender to 0.10.1
      * enable coverage for travis
      * comment out phantom 2 setup for travis
      * use custom fork of ember-cli-blanket

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,41 @@
+/* global QUnit */
 import resolver from './helpers/resolver';
+import Pretender from 'pretender';
 import {
   setResolver
 } from 'ember-qunit';
 
 setResolver(resolver);
+
+var server;
+var usePretender = false;
+var usePassthrough = false;
+QUnit.begin(function() {
+  console.log('begin tests');
+
+  if (usePretender) {
+    console.log('using pretender');
+    server = new Pretender(function() {
+      if (usePassthrough) {
+        this.post('/write-blanket-coverage', this.passthrough);
+      } else {
+        console.log('NOT using passthrough');
+      }
+    });
+    server.handledRequest = function(verb, path) {
+      console.log('handled',verb,path);
+    };
+    server.unhandledRequest = function(verb, path) {
+      console.log('UN handled',verb,path);
+    };
+    server.passthroughRequest = function(verb, path) {
+      console.log('passed through',verb,path);
+    };
+  } else {
+    console.log('not using pretender');
+  }
+});
+
+QUnit.done(function() {
+  console.log('end tests');
+});
