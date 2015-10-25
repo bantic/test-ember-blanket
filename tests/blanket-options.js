f83cc309eb21f775b7bf0a637205c0dd90d17479
Cory Forsyth <cory.forsyth@gmail.com>
2015-10-25 14:55:05 -0400

    initial

---
+++ tests/blanket-options.js
@@ -0,0 +1,18 @@
+/* globals blanket, module */
+
+var options = {
+  modulePrefix: 'test-ember-blanket',
+  filter: '//.*test-ember-blanket/.*/',
+  antifilter: '//.*(tests|template).*/',
+  loaderExclusions: [],
+  enableCoverage: true,
+  cliOptions: {
+    reporters: ['json'],
+    autostart: true
+  }
+};
+if (typeof exports === 'undefined') {
+  blanket.options(options);
+} else {
+  module.exports = options;
+}
